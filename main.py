a66e2e8a7481c7d4c0a3ae96793be56f5a006b2f
vantru <trutran1992@gmail.com>
2026-05-19 08:59:14 +0700

    remote git repository

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, request
+
+def create_app():   
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        message = os.getenv("MESSAGE", "Hello from Flask on kubenetes")
+
+        return f"""
+        <h3>argocd</h3>
+        <h1 style="color: black;">{message}</h1>
+        """
+    return app
+app = create_app()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8000")), debug=True)
+
